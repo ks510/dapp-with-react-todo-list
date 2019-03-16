cc51497166c90267f5b3edc5c79d7361cceb1f93
Karen Suen <karen@Karens-MacBook-Air.local>
2019-03-16 14:41:39 +0000

    Read taskCount and added CSS for App

--- src/App.js
+++ src/App.js
@@ -11,23 +11,30 @@ class App extends Component {
   async loadBlockchainData() {
     // use provider from Meta mask or default
     const web3 = new Web3(Web3.givenProvider || "http://localhost:8545")
-    const network = await web3.eth.net.getNetworkType()
-    const accounts = await web3.eth.getAccounts();
-    this.setState({ account: accounts[0]});
-    console.log("accounts", accounts[0])
-    //Fetch account
+    //Fetch account and store it in state
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+    const todoList = new web3.eth.Contract(TODO_LIST_ABI, TODO_LIST_ADDRESS)
+    this.setState({ todoList })
+    const taskCount = await todoList.methods.taskCount().call()
+    this.setState({ taskCount })
+
+    console.log("todoList", todoList)
   }
 
   constructor(props) {
     super(props);
-    this.state = { account: '' }
+    this.state = {
+      account: '',
+      taskCount: 0
+    }
   }
 
   render() {
     return (
       <div className="container">
         <h1>Hello World!</h1>
-        <p>Your account: {this.state.account}</p>
+        <p>Task Count: {this.state.taskCount}</p>
       </div>
     );
   }
