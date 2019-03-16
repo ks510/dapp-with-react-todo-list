d030730a3c4bbdcb2e14816375de08353b91c261
Karen Suen <karen@Karens-MacBook-Air.local>
2019-03-16 15:23:06 +0000

    implemented displaying all tasks in client app

--- src/App.js
+++ src/App.js
@@ -18,6 +18,14 @@ class App extends Component {
     this.setState({ todoList })
     const taskCount = await todoList.methods.taskCount().call()
     this.setState({ taskCount })
+    // getting each task from tasks mapping
+    for (var i = 1; i <= taskCount; i++) {
+      const task = await todoList.methods.tasks(i).call()
+      this.setState({
+        tasks: [...this.state.tasks, task]
+      })
+    }
+    console.log("tasks", this.state.tasks);
 
     console.log("todoList", todoList)
   }
@@ -26,15 +34,51 @@ class App extends Component {
     super(props);
     this.state = {
       account: '',
-      taskCount: 0
+      taskCount: 0,
+      tasks: []
     }
   }
 
   render() {
     return (
       <div className="container">
-        <h1>Hello World!</h1>
-        <p>Task Count: {this.state.taskCount}</p>
+        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+          <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="http://www.dappuniversity.com/free-download" target="_blank">Dapp University | Todo List</a>
+          <ul className="navbar-nav px-3">
+            <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+              <small><a className="nav-link" href="#"><span id="account"></span></a></small>
+            </li>
+          </ul>
+        </nav>
+        <div className="container-fluid">
+          <div className="row">
+            <main role="main" className="col-lg-12 d-flex justify-content-center">
+              <div id="loader" className="text-center">
+                <p className="text-center">Loading...</p>
+              </div>
+              <div id="content">
+                <form>
+                  <input id="newTask" type="text" className="form-control" placeholder="Add task..." required/>
+                  <input type="submit" hidden=""/>
+                </form>
+                <ul id="taskList" className="list-unstyled">
+                {this.state.tasks.map((task,key) => { // for every task in state, render this div
+                  return(                             // task = object in array, key = index of object
+                    <div className="taskTemplate" className="checkbox" key={key}>
+                      <label>
+                        <input type="checkbox" />
+                        <span className="content">{task.content}</span>
+                      </label>
+                    </div>
+                  )
+                })}
+                </ul>
+                <ul id="completedTaskList" className="list-unstyled">
+                </ul>
+              </div>
+            </main>
+          </div>
+        </div>
       </div>
     );
   }
